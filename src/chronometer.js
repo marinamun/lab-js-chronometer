a767707152b75b1b033fc5e5f451279c59470d9e
Marina Muñoz <marinacerejidoo@gmail.com>
2023-09-12 16:06:21 +0200

    Finished lab without bonus

--- src/chronometer.js
+++ src/chronometer.js
@@ -1,30 +1,53 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(printTimeCallback) {
     // ... your code goes here
+    //set the interval formula
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+      //we make sure there is a callback. if yes,with each second that is added we send it to the next function
+      //that will update the clock in html.
+      if (printTimeCallback) {
+        printTimeCallback(this.currentTime);
+      } 
+    }, 1000);
+    
   }
 
   getMinutes() {
     // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    // ... your code goes here. calculate the seconds after a minute has passed.
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
     // ... your code goes here
+
+    if (!value[1]) {
+      const valueString = value.toString();
+      return ("0" + valueString).slice(-2);
+    } else {
+      return value.toString();
+    }
   }
 
   stop() {
     // ... your code goes here
+    clearInterval(this.currentTime);
   }
 
   reset() {
     // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
